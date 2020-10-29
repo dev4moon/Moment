c6062bffa57ad858d48a6a828117ede2d9242617
Seungjun Moon <72376931+SeungjunMoon@users.noreply.github.com>
2020-10-29 18:36:57 +0900

    Getting Geo&Weather API

--- clock.js
+++ clock.js
@@ -1,22 +1,20 @@
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector("h1");
 
-function getTime(){
-    const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${
-        hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `0${minutes}` : minutes}:${
-        seconds < 10 ? `0${seconds}` : seconds
-    }`;
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 //10보다 작은 숫자 두자리수로 만들기
 
 function init() {
-    getTime();
-    setInterval(getTime, 1000);
+  getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
